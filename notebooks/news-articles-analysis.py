b063f7c72de15139b89c7a47fb41755d994038c9
tranquanghuy0801 <huytran.quang080199@gmail.com>
2020-04-19 11:27:38 +1000

    Preprocess datetime & header and remove duplicates dataframe

---
+++ notebooks/news-articles-analysis.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import os
+import fnmatch
+import glob
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from preprocess_text import *
+from textblob import TextBlob
+
+def merge_csv_files(directory: str, pattern = "*.csv") -> pd.DataFrame(): 
+
+	# Initialize DataFrame
+	df = pd.DataFrame()
+
+	# List all files in this directory 
+	for root, dirs, files in os.walk(directory):
+		for basename in files:
+			if fnmatch.fnmatch(basename, pattern):
+				filename = os.path.join(root, basename)
+				print(filename)
+				csv_df = pd.read_csv(filename,header=None,names=['link','header','article','author','date'])
+				# Merge all CSV files
+				df = pd.concat([csv_df,df],ignore_index=False)
+	
+	return df
+
+def sentiment_score(text: str) -> int:
+	wiki = TextBlob(text)
+	numberOfSentences = 0
+	score = 0
+	sentiment = SentimentIntensityAnalyzer()
+	for sentence in wiki.sentences:	
+		polarity = sentence.sentiment.polarity
+		subjectivity = sentence.sentiment.subjectivity
+		numberOfSentences += 1
+		score += polarity * subjectivity
+		
+	normalized_score = round(score/numberOfSentences,3)
+	return normalized_score
+
+def sentiment_label(score: int) -> str:
+	#Classifying sentence based on score.
+	if score > 0:
+		sentiment_label = "positive"
+	elif score < 0:
+		sentiment_label = "negative"
+	else:
+		sentiment_label = "neutral"
+	return sentiment_label
+
+def preprocess_df(df: pd.DataFrame()) -> pd.DataFrame():
+	# Remove the first row of dataframe
+	df = df.iloc[1:]
+	# Remove duplicated rows with the same link
+	df.drop_duplicates(subset="link",keep=False, inplace=True)
+	# Convert time in datetime 
+	df['date'] = df['date'].apply(lambda text: convert_datetime(text))
+	df['header'] = df['header'].apply(lambda text: preprocess_text(text))
+	df['score'] = df['header'].apply(lambda text: sentiment_score(text))
+	df['label'] = df['score'].apply(lambda text: sentiment_label(text))
+
+	return df
+
+
+abc_df = merge_csv_files('../data/abc_10_Apr_2020')
+abc_df = preprocess_df(abc_df)
+print(abc_df.head(10))
+print(abc_df.shape)
+# print(sentiment_score("Pearls out, protests in: This generation of grandmothers is kicking the tired cliches aside"))
--- notebooks/preprocess_text.py
+++ notebooks/preprocess_text.py
@@ -1,88 +1,28 @@
-import re, string, unicodedata
+import re
 import nltk
-import contractions
-import inflect
 import os
 from nltk import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
+from dateutil.parser import parse
 
-def replace_contractions(text):
-    """Replace contractions in string of text"""
-    return contractions.fix(text)
+def convert_datetime(text):
+	"""Replace date format June 06, 2020 to 2020-06-06"""
+	return str(parse(text).date())
 
-
-def remove_URL(sample):
-    """Remove URLs from a sample string"""
-    return re.sub(r"http\S+", "", sample)
-
-
-def remove_non_ascii(words):
-    """Remove non-ASCII characters from list of tokenized words"""
-    new_words = []
-    for word in words:
-        new_word = unicodedata.normalize('NFKD', word).encode(
-            'ascii', 'ignore').decode('utf-8', 'ignore')
-        new_words.append(new_word)
-    return new_words
-
-
-def to_lowercase(words):
-    """Convert all characters to lowercase from list of tokenized words"""
-    new_words = []
-    for word in words:
-        new_word = word.lower()
-        new_words.append(new_word)
-    return new_words
-
-
-def remove_punctuation(words):
-    """Remove punctuation from list of tokenized words"""
-    new_words = []
-    for word in words:
-        new_word = re.sub(r'[^\w\s]', '', word)
-        if new_word != '':
-            new_words.append(new_word)
-    return new_words
-
-
-def replace_numbers(words):
-    """Replace all interger occurrences in list of tokenized words with textual representation"""
-    p = inflect.engine()
-    new_words = []
-    for word in words:
-        if word.isdigit():
-            new_word = p.number_to_words(word)
-            new_words.append(new_word)
-        else:
-            new_words.append(word)
-    return new_words
-
-
-def remove_stopwords(words):
-    """Remove stop words from list of tokenized words"""
-    new_words = []
-    for word in words:
-        if word not in stopwords.words('english'):
-            new_words.append(word)
-    return new_words
-
-
-def normalize(words):
-    words = remove_non_ascii(words)
-    words = to_lowercase(words)
-    words = remove_punctuation(words)
-    words = replace_numbers(words)
-    words = remove_stopwords(words)
-    return words
-
-
-def preprocess(sample):
-    sample = remove_URL(sample)
-    sample = replace_contractions(sample)
-    # Tokenize
-    words = nltk.word_tokenize(sample)
-
-    # Normalize
-    words = normalize(sample)
-    words = ' '.join(word for word in words)
-    return words
+def preprocess_text(sample):
+	# 1. Remove non-letters        
+    letters_only = re.sub("[^a-zA-Z]", " ", sample) 
+    #
+    # 2. Convert to lower case, split into individual words
+    words = letters_only.lower().split()                             
+    #
+    # 3. In Python, searching a set is much faster than searching
+    #   a list, so convert the stop words to a set
+    stops = set(stopwords.words("english"))                  
+    # 
+    # 4. Remove stop words
+    meaningful_words = [w for w in words if not w in stops]   
+    #
+    # 5. Join the words back into one string separated by space, 
+    # and return the result.
+    return( " ".join( meaningful_words ))
